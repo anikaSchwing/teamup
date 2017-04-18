059ab552b792af5a45bbe8355141d6afb5fd3393
anikaSchwing <anikaschwing@gmail.com>
2017-04-18 17:40:58 +0200

    fixed associations added seeds

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,4 @@
 class Match < ApplicationRecord
-  belongs_to :student_1, :class_name => 'User'
-  belongs_to :student_2, :class_name => 'User'
+  belongs_to :student_1, :class_name => 'User', :foreign_key => 'student_1'
+  belongs_to :student_2, :class_name => 'User', :foreign_key => 'student_2'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ User.delete_all
 puts "Deleted all the users"
 
 User.create!(email: "admin@b.com", password: "testing", admin: true)
-User.create!(email: "student@b.com", password: "testing", admin: false)
-User.create!(email: "student2@b.com", password: "testing", admin: false)
+student1 = User.create!(email: "student@b.com", password: "testing", admin: false)
+student2 = User.create!(email: "student2@b.com", password: "testing", admin: false)
 puts "Created new users"
 
-Match.create!(day: "2017-04-03T00:00:00+00:00", student_1: 1 , student_2: 0)
+Match.create!(day: "2017-04-03T00:00:00+00:00", student_1: student1, student_2: student2)
 puts "Created matches"
