24d3763e5df6d23ba99f59273b545d03a23e45ab
anikaSchwing <anikaschwing@gmail.com>
2017-04-18 19:12:59 +0200

    added names to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ User.delete_all
 puts "Deleted all the users"
 
 User.create!(email: "admin@b.com", password: "testing", admin: true)
-student1 = User.create!(email: "student@b.com", password: "testing", admin: false)
-student2 = User.create!(email: "student2@b.com", password: "testing", admin: false)
-student3 = User.create!(email: "student3@b.com", password: "testing", admin: false)
+student1 = User.create!(name: "bert", email: "student@b.com", password: "testing", admin: false)
+student2 = User.create!(name: "ernie", email: "student2@b.com", password: "testing", admin: false)
+student3 = User.create!(name: "big bird", email: "student3@b.com", password: "testing", admin: false)
 puts "Created new users"
 
 Match.create!(day: "2017-04-03T00:00:00+00:00", student_1: student1, student_2: student2)
