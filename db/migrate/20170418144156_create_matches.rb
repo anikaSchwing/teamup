8a63542d26c03c7d64aad8e6918bb51cd50aa314
anikaSchwing <anikaschwing@gmail.com>
2017-04-18 17:10:00 +0200

    created match model and associations

---
+++ db/migrate/20170418144156_create_matches.rb
@@ -0,0 +1,10 @@
+class CreateMatches < ActiveRecord::Migration[5.0]
+  def change
+    create_table :matches do |t|
+      t.datetime :day
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418141759) do
+ActiveRecord::Schema.define(version: 20170418150420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "matches", force: :cascade do |t|
+    t.datetime "day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "student_1"
+    t.integer  "student_2"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
