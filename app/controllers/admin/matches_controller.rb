21936e34309da413100cac9dcb5d286f1cd6c88f
anikaSchwing <anikaschwing@gmail.com>
2017-04-19 16:02:46 +0200

    reset because everything was broken

--- app/controllers/admin/matches_controller.rb
+++ app/controllers/admin/matches_controller.rb
@@ -18,3 +18,49 @@ class Admin::MatchesController < ApplicationController
   end
 
 end
+
+private
+    def matchStudents()
+      matched_students = []
+      all_students = User.where(admin:false)
+
+      all_students.each do |student|
+        if (!matched_students.flatten.include?(student))
+          first_student = student
+          second_student = findMatch(first_student, matched_students)
+
+          matched_students << [first_student, second_student]
+        end
+      end
+
+      return matched_students
+    end
+
+    def findMatch(first_student, matched_students)
+      students = User.where(admin:false).where.not(id: first_student.id)
+      second_student = students[rand(students.length)]
+      if (!matched_students.flatten.include?(second_student))
+        return second_student
+      else
+        findMatch(first_student, matched_students)
+      end
+    end
+
+    def createMatches(day)
+      matched_students = matchStudents()
+
+      matched_students.each do |students|
+        new_match = Match.create(day: day, student_1: students[0], student_2: students[1])
+      end
+
+      redirect_to admin_matches_path, notice: "Matches created for #{day.to_date}"
+    end
+
+    def removeExistingMatches(day)
+      todays_matches = Match.where(day: day)
+      if(todays_matches.length > 0)
+        todays_matches.each do |match|
+          match.destroy
+        end
+      end
+    end
