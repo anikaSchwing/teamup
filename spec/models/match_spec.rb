0696e335b47f67a242f5cb3a49914d2f51e820bc
anikaSchwing <anikaschwing@gmail.com>
2017-04-19 09:20:50 +0200

    created match model test

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "association with students" do
+    let(:student) { create :user }
+    let(:another_student) { create :user }
+
+    let(:match) { create :match, student_1: student, student_2: another_student }
+
+    it "belongs to two students" do
+      expect(match.student_1).to eq(student)
+      expect(match.student_2).to eq(another_student)
+    end
+  end
+
+
 end
