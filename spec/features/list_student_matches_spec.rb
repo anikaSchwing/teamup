6f049caf48e6bfcb2ea8b33ad16c41a9083363ad
anikaSchwing <anikaschwing@gmail.com>
2017-04-20 15:24:46 +0200

    added test user sees list of all their matches

---
+++ spec/features/list_student_matches_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "Current student viewing matches list" do
+  before { login_as student }
+
+  let(:student) { create :user }
+  let(:student2) { create :user }
+  let(:student3) { create :user }
+  let(:student4) { create :user }
+
+  let!(:match) { create :match, student_1: student, student_2: student2 }
+  let!(:match2) { create :match, student_1: student3, student_2: student2 }
+  let!(:match3) { create :match, student_1: student, student_2: student4 }
+
+  it "shows all students matches" do
+    visit matches_url
+
+    expect(page).to have_text("You are matched with #{student2.name}")
+    expect(page).to have_text("You are matched with #{student4.name}")
+  end
+
+  it "does not show other students matches" do
+    visit matches_url
+
+    expect(page).not_to have_text("You are matched with #{student3.name}")
+  end
+end
