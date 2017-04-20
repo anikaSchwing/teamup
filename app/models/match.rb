de524524459cd06e63f5b8c13e98148cab392e7a
anikaSchwing <anikaschwing@gmail.com>
2017-04-20 13:31:41 +0200

    added validations

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,4 +2,5 @@ class Match < ApplicationRecord
   belongs_to :student_1, :class_name => 'User'
   belongs_to :student_2, :class_name => 'User'
 
+  validates :day, :student_1, :student_2, presence: true
 end
